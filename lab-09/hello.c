b36bd13bd322800db46eef820e8a8c9200da9ae2
Wahaj404 <wahajmaqsood20@gmail.com>
2021-04-28 12:02:47 +0500

    lab-09

---
+++ lab-09/hello.c
@@ -0,0 +1,12 @@
+#include <linux/kernel.h> /* Needed for KERN_INFO */
+#include <linux/module.h> /* Needed by all modules */
+int init_module(void) {
+    printk(KERN_INFO "hello [INFO] Hellow World! \n");
+    /*
+     * A non 0 return means init_module failed; module can't be loaded.
+     */
+    return 0;
+}
+void cleanup_module(void) {
+    printk(KERN_INFO " hello [INFO] Goodbye World! \n ");
+}
